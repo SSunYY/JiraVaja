4608cfc7632a4aaf34e3e2c417446a38bc2f557f
Martin Jevševar <martin.jevsevar123@gmail.com>
2025-05-05 12:28:06 +0200

    Dodane vse potrebne datoteke za začetek implementacije.

---
+++ LinearQueue.h
@@ -0,0 +1,14 @@
+//
+// Created by Martini on 3. 05. 2025.
+//
+
+#ifndef NALOGA7_1_POINTPRIMER_LINEARQUEUE_H
+#define NALOGA7_1_POINTPRIMER_LINEARQUEUE_H
+
+#include <string>
+
+using namespace std;
+
+
+
+#endif //NALOGA7_1_POINTPRIMER_LINEARQUEUE_H
